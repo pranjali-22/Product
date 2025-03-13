597cea9061fbfc703bd7c1772d74e198a6ca166d
Pranjali <pranjalidas2201@gmail.com>
2025-03-13 13:50:26 -0700

    frontend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -42,6 +42,9 @@ export const updateProduct = async (req,res) => {
 }
 export const deleteProduct = async (req,res) => {
     const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message: "Id not found"});
+    }
     try{
         await Product.findByIdAndDelete(id);
         res.status(200).json({success:true, message:"Product deleted"});
--- backend/routes/product.routejs.js
+++ backend/routes/product.routejs.js
@@ -6,8 +6,8 @@ export default router;
 
 router.get("/", getProducts);
 router.post("/", createProduct);
-router.put("/:id", updateProduct()); // patch - when updating some fields // put - when updating some fields
+router.put("/:id", updateProduct); // patch - when updating some fields // put - when updating some fields
 router.delete("/:id", deleteProduct);
 
-export default router;
+// export default router;
 
