f66f96082e9efae7b6b4cad1bc4cbadecc144e28
Pranjali <pranjalidas2201@gmail.com>
2025-03-13 12:27:34 -0700

    connecting database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
 dotenv.config();
 const app = express();
+
 app.get("/products", (req,res) => {
     res.send("Sever is ready123");
 })
-console.log("here");
+
 console.log(process.env.MONGO_URI);
 app.listen(5000, () => {
     connectDB();
